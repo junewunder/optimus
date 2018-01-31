1febb5febfe5b82d03ecd301d63e418368bf22ee
Jacob Wunder <jacob@jacobwunder.com>
2018-01-31 15:47:25 -0500

    added "x" woops lol

--- optimus.py
+++ optimus.py
@@ -1,5 +1,5 @@
 # CHANGE HERE
-string = 'lenny'
+string = 'jacob'
 
 
 def is_prime(n):
@@ -17,13 +17,10 @@ numbers = {
     '8': 8, '9': 9, 'a': 10, 'b': 11, 'c': 12, 'd': 13, 'e': 14,
     'f': 15, 'g': 16, 'h': 17, 'i': 18, 'j': 19, 'k': 20,
     'l': 21, 'm': 22, 'n': 23, 'o': 24, 'p': 25, 'q': 26, 'r': 27,
-    's': 28, 't': 29, 'u': 30, 'v': 31, 'w': 32, 's': 33, 'y': 34, 'z': 35
+    's': 28, 't': 29, 'u': 30, 'v': 31, 'w': 32, 'x': 33, 'y': 34, 'z': 35
 }
 
-
 string = string[::-1]
-
-# TODO: is the bee movie script prime????
 
 highestbase = -1
 for l in string:
